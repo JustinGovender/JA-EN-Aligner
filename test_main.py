9f30c16e7a2ec5cafb58d4b263fa06159a4f642e
Govender, Justin <jmg66@students.waikato.ac.nz>
2020-01-30 14:33:15 +0900

    Fixed test

--- test_main.py
+++ test_main.py
@@ -12,7 +12,8 @@ def test_2():
     test_text = ''
     full_text = environ.get('env_source_text')
     processed_text = preprocess(full_text, 'ja').split('\n')
-    while test_text == '' :
-        for line in processed_text:
-            test_text = line
+    for line in processed_text:
+        test_text = line
+        if test_text != '':
+            break
     assert test_text == '発明の名称取付装置および電子機器'
