ce8103e9759c59ca1067fb231623828228f2bdce
Govender, Justin <jmg66@students.waikato.ac.nz>
2020-01-30 15:23:15 +0900

    Trying to get test to work.

--- test_main.py
+++ test_main.py
@@ -11,9 +11,10 @@ def test_1():
 def test_preprocessing_removes_header():
     test_text = ''
     full_text = environ.get('env_source_text')
-    for line in preprocess(full_text, 'ja').split('\n'):
-        test_text = line
-        if test_text != '':
+    full_text = preprocess(full_text, 'ja').split('\n')
+    for i in enumerate(full_text):
+        if full_text[i] != '':
+            test_text = full_text[i]
             break
     assert test_text == '発明の名称取付装置および電子機器'
 
